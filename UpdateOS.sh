d8cdc4b16e0561dec056576735439b7f3fbb1c4b
BreX900 <44398785+BreX900@users.noreply.github.com>
2018-11-18 13:57:00 +0100

    Add UpdateOS

---
+++ UpdateOS.sh
@@ -0,0 +1,27 @@
+echo df -h --total;
+df -h --total;
+echo " ";
+echo Start update?
+read TMP;
+
+echo sudo apt update;
+sudo apt update;
+echo " ";
+echo sudo apt upgrade;
+sudo apt upgrade;
+echo " ";
+echo sudo apt dist-upgrade;
+sudo apt dist-upgrade;
+echo " ";
+echo sudo apt autoclean;
+sudo apt autoclean;
+echo " ";
+echo sudo apt autoremove;
+sudo apt autoremove;
+
+echo "\n\n";
+echo df -h --total;
+df -h --total;
+echo " ";
+echo Finish, press to exit...;
+read TMP;
