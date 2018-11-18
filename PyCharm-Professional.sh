f9cde4aa1c11d1880095278d4ba2e13efb29fb97
BreX900 <44398785+BreX900@users.noreply.github.com>
2018-11-18 13:36:31 +0100

    Add files via upload

---
+++ PyCharm-Professional.sh
@@ -0,0 +1,20 @@
+echo sudo add-apt-repository ppa:viktor-krivak/pycharm;
+sudo add-apt-repository ppa:viktor-krivak/pycharm;
+
+echo sudo apt update;
+sudo apt update;
+
+echo sudo apt install pycharm-professional;
+sudo apt install pycharm-professional;
+
+echo sudo dpkg -i ~/Downloads/pycharm-professional_*.deb;
+sudo dpkg -i ~/Downloads/pycharm-professional_*.deb;
+
+echo sudo apt -f install;
+sudo apt -f install;
+
+echo Unistallc command: \"sudo apt remove --autoremove pycharm-professional\";
+
+echo " ";
+echo "Finish, press to exit...";
+read TMP;
