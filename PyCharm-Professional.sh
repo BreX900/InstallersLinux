b7c029b2e84f74210699bb45e0001fe348922d57
BreX900 <44398785+BreX900@users.noreply.github.com>
2018-11-20 22:59:32 +0100

    Update PyCharm-Professional.sh

--- PyCharm-Professional.sh
+++ PyCharm-Professional.sh
@@ -7,18 +7,9 @@ sudo apt update;
 echo sudo apt install pycharm-professional;
 sudo apt install pycharm-professional;
 
-echo "Download PyCharm-Professional: https://www.jetbrains.com/pycharm/";
-echo "Press to continue...";
-read TMP;
-
-echo sudo dpkg -i ~/Downloads/pycharm-professional_*.deb;
-sudo dpkg -i ~/Downloads/pycharm-professional_*.deb;
-
-echo sudo apt -f install;
-sudo apt -f install;
-
 echo Unistallc command: \"sudo apt remove --autoremove pycharm-professional\";
 
 echo " ";
+echo "Install succeful!";
 echo "Finish, press to exit...";
 read TMP;
