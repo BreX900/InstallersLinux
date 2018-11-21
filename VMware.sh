b7ecd30907d1c7ff90e9e3b2b6517b5db1d5cf30
BreX900 <44398785+BreX900@users.noreply.github.com>
2018-11-21 15:39:32 +0100

    Add files via upload

---
+++ VMware.sh
@@ -0,0 +1,24 @@
+echo "The script must be in the download folder";
+echo "Press any key for continue or 'Ctrl+c to close and move in download folder";
+read TMP;
+
+echo sudo apt install build-essential;
+sudo apt install build-essential;
+
+echo " ";
+echo " ";
+echo "Download VMware from: https://www.vmware.com/products/workstation-player.html";
+echo "Press any key to continue...";
+read TMP;
+
+echo chmod +x VMware-Player*;
+chmod +x VMware-Player*;
+
+echo sudo VMware-Player*;
+sudo ./VMware-Player*;
+
+echo " ";
+echo " ";
+echo " ";
+echo "Finish installation... Press any key to exit...";
+read TMP;
